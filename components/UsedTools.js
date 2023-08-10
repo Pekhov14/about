8340e01a58171c95b804b739a8e2fe3dcc5adfb8
pekhov <pehovanton21@gmail.com>
2023-08-10 22:57:20 +0300

    Used tools

---
+++ components/UsedTools.js
@@ -0,0 +1,12 @@
+const UsedTools = () => {
+    return (
+        <div>
+            <ul>
+                <li>php</li>
+                <li>js</li>
+            </ul>
+        </div>
+    )
+}
+
+export {UsedTools}
