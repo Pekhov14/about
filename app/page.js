58a73ad81547376f5580a42959513b4283430fe1
pekhov <pehovanton21@gmail.com>
2023-08-11 23:47:02 +0300

    First screen

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
+import {FirstScreen} from "@/components/layout/FirstScreen";
+import {About} from "@/components/layout/About";
 import {Experience} from "@/components/layout/Experience";
 import {UsedTools} from "@/components/layout/UsedTools";
 import {Education} from "@/components/layout/Education";
 import {Contacts} from "@/components/layout/Contacts";
-import {About} from "@/components/layout/About";
 
 
 export default function Home() {
     return (
         <main>
+            <FirstScreen/>
             <About/>
             <Experience/>
             <UsedTools/>
---
+++ components/layout/FirstScreen.jsx
@@ -0,0 +1,13 @@
+const FirstScreen = () => {
+    return (
+        <section className="content content--centered">
+            <div className="content__title">
+                <span className="content__title-pre content__title-pre--offset">Anthony</span>
+                <h1 className="content__title-main">Pekhov</h1>
+            </div>
+            <div className="content__scroll">Scroll, if you wish</div>
+        </section>
+    )
+}
+
+export {FirstScreen}
