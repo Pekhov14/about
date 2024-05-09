a71356e0187c880625ad82088ccec6890b82240f
pekhov <pehovanton21@gmail.com>
2024-05-10 00:18:25 +0300

    image tags 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import {Menu} from '@/components/layout/Menu';
 import { Metadata } from 'next';
-import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -47,15 +46,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="apple-touch-icon" href="/assets/img/og-card.png" />
-        <link rel="shortcut" href="/assets/img/og-card.png" />
-        <link rel="msapplication-TileImage" href="/assets/img/og-card.png" />
-      </Head>
-      <body className={inter.className}>
-      <Menu />
-      {children}
-      </body>
+    <body className={inter.className}>
+    <Menu/>
+    {children}
+    <link rel="apple-touch-icon" href="/assets/img/og-card.png"/>
+    <link rel="shortcut" href="/assets/img/og-card.png"/>
+    <link rel="msapplication-TileImage" href="/assets/img/og-card.png"/>
+    </body>
     </html>
   )
 }
