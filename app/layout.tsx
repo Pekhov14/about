538fd71465d6f3b56efe09da624d090aa674c809
pekhov <pehovanton21@gmail.com>
2024-05-09 23:45:51 +0300

    OG tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,46 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import {Menu} from '@/components/layout/Menu';
+import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'About Anthony Pekhov',
-  description: 'Software developer',
-  keywords: 'backend, developer, symfony, opencart, php'
+const title = 'Anthony Pekhov - Software Developer';
+const description = 'Hello, I\'m Anthony. 👋 I An software developer specializing in backend I usually use PHP (OpenCart, Symfony, Laravel) and a little frontend in JS (React, Next).';
+const siteUrl = 'https://anton-pekhov.vercel.app/';
+const imgAlt = 'Preview image for Anthony Pekhov'
+
+export const metadata: Metadata = {
+    title: title,
+    description: description,
+    keywords: 'backend, developer, symfony, opencart, php, js',
+    openGraph: {
+        title: title,
+        description: description,
+        url: siteUrl,
+        siteName: "About Anthony Pekhov",
+        type: 'website',
+        images: [
+            {
+                url: `${siteUrl}assets/img/og-card.png`,
+                secureUrl: `${siteUrl}assets/img/og-card.png`,
+                width: 690,
+                height: 326,
+                alt: imgAlt,
+            }
+        ]
+    },
+    twitter: {
+        card: 'summary_large_image',
+        site: '@anthony_pekhov',
+        title: title,
+        description: description,
+        creator: '@anthony_pekhov',
+        images: {
+            url: `${siteUrl}assets/img/og-card.png`,
+            alt: imgAlt,
+        }
+    },
 }
 
 export default function RootLayout({ children }) {
